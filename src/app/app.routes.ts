5e760248f9123c05e282af59aa45abe8f7db406d
Silverfeelin <Silverfeelin@gmail.com>
2025-01-28 18:51:29 +0100

    Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { TrackerComponent } from './components/tracker/tracker.component';
+
+export const routes: Routes = [
+  {
+    path: '', component: TrackerComponent,
+  }
+];
---
+++ src/app/components/tracker/tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracker',
+  imports: [],
+  standalone: true,
+  templateUrl: './tracker.component.html',
+  styleUrl: './tracker.component.scss'
+})
+export class TrackerComponent {
+
+}
