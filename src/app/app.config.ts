a309a9d980bcf90c3e28d002e7ec0a38fc9c77b4
Silverfeelin <Silverfeelin@gmail.com>
2025-02-05 16:23:36 +0100

    Infographic test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule } from '@angular/platform-browser';
+
+export class HammerConfig extends HammerGestureConfig {
+  override overrides = {
+    swipe: { direction: Hammer.DIRECTION_ALL }
+  };
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(HammerModule),
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: HammerConfig
+    }
+  ]
 };
---
+++ src/app/components/infographic/infographic.component.ts
@@ -0,0 +1,77 @@
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, inject, signal, ViewChild } from '@angular/core';
+import { DomSanitizer, HammerModule, SafeHtml } from '@angular/platform-browser';
+import L from 'leaflet';
+import 'hammerjs';
+import { OverlayComponent } from "../overlay/overlay.component";
+
+@Component({
+  selector: 'app-infographic',
+  templateUrl: './infographic.component.html',
+  styleUrl: './infographic.component.scss',
+  imports: [HammerModule, OverlayComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InfographicComponent implements AfterViewInit {
+  @ViewChild('divMap', { static: true }) mapDiv!: ElementRef<HTMLDivElement>;
+
+  map!: L.Map;
+
+  private readonly _http = inject(HttpClient);
+  private readonly _changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly _domSanitizer = inject(DomSanitizer);
+
+  svgContent = signal<SafeHtml>('');
+  isOverlayVisible = true;
+
+  constructor(
+  ) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this._http.get('/assets/external/maps/infographic/test.svg', { responseType: 'text' }).subscribe((svg) => {
+      // Replace relative paths with asset path.
+      svg = svg.replace(/xlink:href="([^"]+)"/g, (match, p1) => {
+        return `xlink:href="/assets/external/maps/infographic/${p1}"`;
+      }).replace(/\.png/g, '.webp');
+
+      const sanitizedSvg = this._domSanitizer.bypassSecurityTrustHtml(svg);
+      this.svgContent.set(sanitizedSvg);
+    });
+  }
+
+  onMapClick(event: MouseEvent): void {
+    // get element at cursor
+    const element = document.elementFromPoint(event.clientX, event.clientY);
+    if (element?.tagName === 'path') {
+      this.selectPolygon(element as SVGPathElement);
+    }
+
+  }
+
+  onSwipeLeft(e: Event): void {
+    alert('Swipe left');
+    console.log(e);
+  }
+
+  onSwipeRight(e: Event): void {
+    alert('Swipe right');
+    console.log(e);
+  }
+
+  onSwipeUp(e: Event): void {
+    alert('Swipe up');
+    console.log(e);
+  }
+
+  onSwipeDown(e: Event): void {
+    alert('Swipe down');
+    console.log(e);
+  }
+
+  selectPolygon(path: SVGPathElement): void {
+    const description = path.dataset['description'];
+    alert(`Select polygon: ${description}`);
+  }
+}
